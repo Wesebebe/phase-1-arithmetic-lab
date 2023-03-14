8d9635bbcfe59d71345d93e94e4770a7dd2b5f75
+ <+>
2023-03-14 12:41:55 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+/*function multiply(num1, num2){
+    //let result = num1 * num2;
+    /*if (result < 62){
+        return Math.ceil(result);
+    }else{
+        return Math.floor(result);
+    }
 
-// Write your code here
+}*/
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+console.log(multiply)
+
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random)
+
+let num3 = 49
+let num4 = 9
+let mod = num3 % num4
+console.log(mod)
+
+let max = Math.max(9, 13, 20, 4, 11, 3, 17)
+console.log(max)
